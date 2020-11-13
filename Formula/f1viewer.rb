1463c1dc08e006bf2438e8fd02e91c430b5ee552
Jakob <s0muchfrsubtlety@gmail.com>
2020-11-13 18:50:18 +0000

    update f1viewer to 1.4.0

--- Formula/f1viewer.rb
+++ Formula/f1viewer.rb
@@ -5,12 +5,12 @@ class F1viewer < Formula
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/SoMuchForSubtlety/f1viewer/releases/download/v1.3.0/f1viewer_1.3.0_macOS_64-bit.tar.gz"
-    sha256 "af5d3788dee97f771e2b9c3a2244baff424c8efa583235daec6347f4c849db8d"
+    url "https://github.com/SoMuchForSubtlety/f1viewer/releases/download/v1.4.0/f1viewer_1.4.0_macOS_64-bit.tar.gz"
+    sha256 "5679f478399b63f17be3982b706beef539daaf774fca4cd0248c13189a672efe"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/SoMuchForSubtlety/f1viewer/releases/download/v1.3.0/f1viewer_1.3.0_linux_64-bit.tar.gz"
-      sha256 "633a41ac55a43ad6a337a40dd946a992bc28cdf2aef0ec9a80c518b7c8f6394a"
+      url "https://github.com/SoMuchForSubtlety/f1viewer/releases/download/v1.4.0/f1viewer_1.4.0_linux_64-bit.tar.gz"
+      sha256 "765695c5db2109157f7b4e89ad06229715d537789c8715eab462e9ff11c658f1"
     end
   end
 
